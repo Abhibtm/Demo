12a68e48bd3b681eec708ca1ca06fa8ed97d0f95
ani-ruddha <aniruddhadas919@gmail.com>
2023-05-12 22:17:48 +0530

    SS

---
+++ DD/src/test/java/dd/TC1.java
@@ -0,0 +1,14 @@
+package dd;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1 {
+	@Test
+	public void m1()
+	{
+		Reporter.log("SS",true);
+	}
+
+}
